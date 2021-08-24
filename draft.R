4e4660bfc574fecc0a7a261b10c24d20145b57cc
dorothearrr <dorothea.strecker@hu-berlin.de>
2021-08-24 15:42:41 +0200

    retrieving data from crossref; updating jupyter notebooks

--- draft.R
+++ draft.R
@@ -1,42 +1,34 @@
+
+### crossref
+
 library(jsonlite)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
+library(purrr)
 
-base_query <- "https://api.crossref.org/works?query.affiliation="
-name_variants <- list("humboldt+universit%C3%A4t+berlin", "humboldt+university+berlin", "hu+berlin")
-mailto <- "&mailto=janedoe@example.org"
+results <- fromJSON(paste0("https://api.crossref.org/works?query.affiliation=humboldt+universit+berlin&filter=from-pub-date:2015-01-01&cursor=", "*&mailto=janedoe@example.org"))
 
-urls <- paste0(rep(base_query, 3), name_variants, rep(mailto, 3))
-print(urls)
-urls
-
-
-
-results <- fromJSON(paste0(urls[1], "&cursor=*"))
-
-DOIs_1 <- results$message$items$DOI
-publication_year_1 <- unlist(map(results$message$items$published$`date-parts`, 1))
-resource_type_1 <- results$message$items$type
+DF <- data.frame(c(DOIs = list(results$message$items$DOI),
+             publication_year = list(unlist(map(results$message$items$published$`date-parts`, 1))),
+             resource_type = list(results$message$items$type)))
 next_cursor <- results$message$`next-cursor`
 
-while (!is.null(next_cursor)) {
-  results <- fromJSON(paste0(urls[1], "&cursor=", next_cursor))
-  DOIs_1 <- append(DOIs_1, results$message$items$DOI)
-  publication_year_1 <- append(publication_year_1, unlist(map(results$message$items$published$`date-parts`, 1)))
-  resource_type_1 <- append(resource_type_1, results$message$items$type)
+
+while (nrow(DF) < results$message$`total-results`) {
+  results <- fromJSON(paste0("https://api.crossref.org/works?query.affiliation=humboldt+universit+berlin&filter=from-pub-date:2015-01-01&cursor=", next_cursor, "&mailto=jdoe@example.org"))
+  DF <- rbind(DF, data.frame(c(DOIs = list(results$message$items$DOI),
+                     publication_year = list(unlist(map(results$message$items$published$`date-parts`, 1))),
+                     resource_type = list(results$message$items$type))))
   next_cursor <- results$message$`next-cursor`
+  print(round(nrow(DF)/results$message$`total-results`*100, 2))
 }
 
-DOIs_1[10000]
-length(DOIs_1)
-length(publication_year_1)
-length(resource_type_1)
-sum(duplicated(DOIs_1))
+write.csv(DF, "crossref_metadata.csv")
 
-save(DOIs_1, file = "./DOIs.rda")
-save(publication_year_1, file = "./publication_year.rda")
-save(resource_type_1, file = "./resource_type.rda")
-load("DOIs.rda")
-load("publication_year.rda")
-load("resource_type.rda")
+
+
+### Unpaywall
+
+
+DF <- read.csv("crossref_metadata.csv")
+
+table(DF$publication_year)
+table(DF$resource_type)
