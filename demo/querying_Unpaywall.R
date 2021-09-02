954d81399d0ad72066ba10933dd44e9b462085b1
dorothearrr <dorothea.strecker@hu-berlin.de>
2021-09-02 15:25:07 +0200

    updated data

---
+++ demo/querying_Unpaywall.R
@@ -0,0 +1,52 @@
+# querying Unpaywall
+
+# Step 1: load packages
+#install.packages("jsonlite")
+library(jsonlite)
+
+
+
+# Step 2: loading crossref metadata
+DF_crossref <- read.csv("./data/crossref_metadata.csv", row.names = "X")
+
+
+
+# Step 3: build query URLs
+URLs <- paste0("https://api.unpaywall.org/v2/", DF_crossref$DOIs, "?email=jdoe@example.org")
+
+
+
+# Step 4: exploring the results
+results <- fromJSON(URLs[1])
+
+results$is_oa
+
+results$oa_status
+
+
+
+
+# retrieving Open Access information from Unpaywall
+# DO NOT RUN THIS!
+#DF_Unpaywall <- data.frame(c(DOIs = character(),
+#                             is_oa = logical(),
+#                             oa_status = character()))
+
+#for (i in 1:nrow(DF_crossref)) {
+#  results <- fromJSON(URLs[i])
+#  DF_Unpaywall[i,1] <- DF_crossref[i,1]
+#  DF_Unpaywall[i,2] <- results$is_oa
+#  DF_Unpaywall[i,3] <- results$oa_status
+#  }
+
+
+
+# EXERCISE: try the package roadoi
+#install.packages("roadoi")
+library(roadoi)
+
+results <- roadoi::oadoi_fetch(dois = DF_crossref[1:10,2], email = "jdoe@example.org")
+names(results)
+
+results$is_oa
+results$journal_is_oa
---
+++ demo/querying_crossref.R
@@ -0,0 +1,74 @@
+# querying crossref
+
+
+# Step 1: load packages
+#install.packages("jsonlite")
+#install.packages("purrr")
+#install.packages("dplyr")
+library(jsonlite)
+library(purrr)
+library(dplyr)
+
+
+
+# Step 2: specify the query parameters
+query <- "https://api.crossref.org/works?query.affiliation=humboldt+universit+berlin&filter=from-pub-date:2015-01-01"
+
+
+
+# Step 3: politely using APIs
+mailto <- "&mailto=jdoe@example.org"
+
+
+
+# Step 4: exploring the results
+results <- jsonlite::fromJSON(paste0(query, mailto))
+
+results$message$items$DOI
+results$message$items$published
+results$message$items$type
+results$message$items$author
+
+results$message$`total-results`
+
+
+
+# Step 5: retrieving all DOIs matching the query
+# DO NOT RUN THIS!
+
+#results <- fromJSON(paste0("https://api.crossref.org/works?query.affiliation=humboldt+universit+berlin&filter=from-pub-date:2015-01-01&cursor=", "*&mailto=janedoe@example.org"))
+
+#DF <- data.frame(c(DOIs = list(results$message$items$DOI),
+#                   publication_year = list(unlist(map(results$message$items$published$`date-parts`, 1))),
+#                   resource_type = list(results$message$items$type),
+#                   affiliation = paste(unique(unlist(lapply(results$message$items$author, function(x) x %>% select(affiliation)))), collapse = " _AND_ ")))
+
+#next_cursor <- results$message$`next-cursor`
+
+#while (nrow(DF) < results$message$`total-results`) {
+#  results <- fromJSON(paste0("https://api.crossref.org/works?query.affiliation=humboldt+universit+berlin&filter=from-pub-date:2015-01-01&cursor=", next_cursor, "&mailto=janedoe@example.org"))
+  
+#  DF <- rbind(DF,  data.frame(c(DOIs = list(results$message$items$DOI),
+#                                publication_year = list(unlist(map(results$message$items$published$`date-parts`, 1))),
+#                                resource_type = list(results$message$items$type),
+#                                affiliation = tryCatch({paste(unique(unlist(lapply(results$message$items$author, function(x) x %>% select(affiliation)))), collapse = " _AND_ ")},  error = function(e) {NA}))))
+  
+#  next_cursor <- results$message$`next-cursor`
+#}
+
+
+
+# EXERCISE: try the package rcrossref
+#install.packages("rcrossref")
+library(rcrossref)
+
+DOI_sample <- read.csv("./data/DOI_sample.csv", row.names = "X")
+
+DF_citations <- data.frame(c(doi = character(),
+                             count = numeric()))
+for (i in 1:nrow(DOI_sample)) {
+  DF_citations <- rbind(DF_citations, cr_citation_count(doi = DOI_sample[i,]))
+}
+
+print(DF_citations)
+summary(DF_citations$count)
